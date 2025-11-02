b362c59dc0c30e02b45e68c756f5078f7fc9a047
gilangandy <gilangandy24@gmail.com>
2025-11-02 17:50:53 +0700

    First Commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { ProductCard } from './component/ProductCard';
+
+
+function App() {
+    return <div className="App">
+        <ProductCard>
+            
+        </ProductCard>
+    </div>
+}
+
+export default App;
